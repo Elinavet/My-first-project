b1d44df3e0b5e01d5cd6f0e3324631df57fb4964
Elinavet <freeefauna@gmail.com>
2024-10-14 12:31:52 +0100

    Add GET /api endpoint for API documentation

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const { getAllTopics } = require('./controllers/topics.controller');
 const app = express();
+const endpoints = require('./endpoints.json')
 
 
 app.get('/api/topics', getAllTopics);
+
+app.get('/api', (req, res, next) => {
+    res.status(200).send(endpoints);
+});
 
 app.use((req, res, next) => {
     res.status(404).send({ msg: 'Path not found' });
