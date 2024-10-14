33deef2c8693167dcf5dc01ea5b91b03505d94dc
Elinavet <freeefauna@gmail.com>
2024-10-14 16:19:23 +0100

    Add GET /api/articles/:article_id endpoint

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 const express = require('express');
 const { getAllTopics } = require('./controllers/topics.controller');
+const { getArticleById } = require('./controllers/articles.controller');
+const endpoints = require('./endpoints.json');
+
 const app = express();
-const endpoints = require('./endpoints.json')
 
 
 app.get('/api/topics', getAllTopics);
-
-app.get('/api', (req, res, next) => {
+app.get('/api', (req, res) => {
     res.status(200).send(endpoints);
 });
+app.get('/api/articles/:article_id', getArticleById);
+
 
 app.use((req, res, next) => {
-    res.status(404).send({ msg: 'Path not found' });
+    const err = new Error('Path not found');
+    err.status = 404;
+    next(err); 
+});
+
+
+app.use((err, req, res, next) => {
+    
+    if (err.status) {
+        return res.status(err.status).send({ msg: err.msg || err.message || 'Error' });
+    }
+    next(err); 
 });
 
 app.use((err, req, res, next) => {
-    if (err.status) {
-        res.status(err.status).send({ msg: err.msg });
-    } else {
-        console.error(err);
-        res.status(500).send({ msg: 'Internal Server Error' });
+    
+    if (err.code === '22P02') {
+        return res.status(400).send({ msg: 'Invalid article ID' });
     }
+    next(err); 
+});
+
+app.use((err, req, res, next) => {
+    
+    console.error(err);
+    res.status(500).send({ msg: 'Internal Server Error' });
 });
 
 module.exports = app;
