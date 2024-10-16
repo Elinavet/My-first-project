0a740851386722e685a5af401dd95417650ab725
Elinavet <freeefauna@gmail.com>
2024-10-16 09:44:17 +0100

    Add GET /api/users endpoint

---
+++ models/users.model.js
@@ -0,0 +1,11 @@
+const db = require('../db/connection');
+
+exports.fetchAllUsers = () => {
+    return db.query(`
+        SELECT username, name, avatar_url
+        FROM users;
+    `)
+    .then((result) => {
+        return result.rows;
+    });
+};
